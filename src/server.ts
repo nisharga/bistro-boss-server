77714103562f9fe0ec1f5fdf6f5037be4986da35
Nisharga Kabir <kabirnisharga@gmail.com>
2023-10-04 14:08:12 +0600

    db-connected

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import app from "./app";
+import config from "./config";
+
+
+const mongoose = require('mongoose');
+
+
+async function boostrap() {
+    try{
+        await mongoose.connect(config.database_url as string);
+        console.log("Database is connected");
+        app.listen(config.port, () => {
+            console.log(`UM app listening on port ${config.port}`)
+          })
+    }
+    catch(err){
+        console.log("Faild to connected DB", err);
+    }
+}
+boostrap();
